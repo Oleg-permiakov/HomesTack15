e85c41c50d2f72416152dd93bc5ea44a8ac7f99b
Oleg <schika145@gmail.com>
2023-10-17 23:11:06 +0500

    Конец

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Проверяет по цифре является ли этот день выходным
+Console.WriteLine("Введите число от 1 до 7");
+string b = Console.ReadLine();
+int a = Convert.ToInt32(b);
+
+if (a == 6 || a == 7) 
+{
+      Console.WriteLine("Да");
+}
+
+ else
+{
+    Console.WriteLine("Нет");
+}
